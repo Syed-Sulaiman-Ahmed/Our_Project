f4968d77baa1a10cbc685340aeda9ca7f841b5af
Roshan yadav <roshanyadav2029@gmail.com>
2026-07-25 22:36:32 +0530

    change in user entity

--- UserService/src/main/java/com/dcl/entity/User.java
+++ UserService/src/main/java/com/dcl/entity/User.java
@@ -3,6 +3,8 @@ package com.dcl.entity;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -34,6 +36,8 @@ public class User {
 	
 	private String password;
 	
+	@CreationTimestamp
+	@Column(updatable = false)
 	private LocalDateTime createdAt;
 	
 	private LocalDateTime updatedAt;
