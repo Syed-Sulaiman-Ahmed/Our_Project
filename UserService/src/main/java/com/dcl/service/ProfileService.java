0b0d4a517ee96d01014bba3e41ae5677e2f67c89
Sushant sah <sahsushant1998@gmail.com>
2026-07-30 10:04:25 +0530

    Usersevice code added

--- UserService/src/main/java/com/dcl/service/ProfileService.java
+++ UserService/src/main/java/com/dcl/service/ProfileService.java
@@ -1,5 +1,11 @@
 package com.dcl.service;
 
+import com.dcl.entity.Profile;
+
 public interface ProfileService {
+	
+	Profile addProfile();
+	
+	
 
 }
--- UserService/src/main/java/com/dcl/service/UserService.java
+++ UserService/src/main/java/com/dcl/service/UserService.java
@@ -1,5 +1,27 @@
 package com.dcl.service;
 
-public interface UserService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.dcl.dto.UserDto;
+import com.dcl.request.RegisterRequest;
+
+
+@Service
+public interface UserService {
+	
+	UserDto addUser(RegisterRequest q);
+	
+//	void updateUser(Integer userId);
+	
+	void deleteUser(Integer userId);
+	
+	UserDto getUserByID(Integer userId);
+	
+	List<UserDto> getAllUser();
+	
+	
+
+	
 }
